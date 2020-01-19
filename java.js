0a2d04cdb860698c3f0bffbef1fad8d89f33b260
Matthew Adair <matthewadair6991@gmail.com>
2020-01-19 15:08:19 -0600

    Added favicon and other minor changes.

--- java.js
+++ java.js
@@ -1,15 +1,13 @@
-const toggleDropdown = (dropdown) => {
-    dropdown.classList.toggle("visible");
-}
 document.addEventListener("DOMContentLoaded", function(){
     const dropdownTriggers = document.getElementsByClassName('dropdown-trigger');
     for (let i=0; i<dropdownTriggers.length; i++) {
         const dropdownTrigger = dropdownTriggers[i];
         const target = dropdownTrigger.dataset.dropdownTarget;
+        
+        const dropdown = document.getElementById(target);
 
-        const dropdown = document.getElementById(target)
         dropdownTrigger.addEventListener('click', function () {
-            toggleDropdown(dropdown);
+            dropdown.classList.toggle("visible");
             dropdownTrigger.classList.toggle("active");
         });
     }
